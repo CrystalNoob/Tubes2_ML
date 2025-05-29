05bcf56d7f3b4b95bbc5da0cd1118d82a6f4845a
MRafliRasyiidin <masraflirasid@gmail.com>
2025-05-29 23:22:14 +0700

    feat: add embedding layer

--- src/lstm.py
+++ src/lstm.py
@@ -3,6 +3,7 @@ from numpy.typing import NDArray
 from typing import Callable, Tuple
 from keras.src.backend import Variable
 import keras
+import tensorflow as tf
 
 class LSTMScratch():
     '''
@@ -35,7 +36,6 @@ class LSTMScratch():
     def __init__(
             self,
             units: int, # neuron
-            # input_feature: NDArray[np.float64], # input
             keras_weight: list[Variable],
             activation: Callable[[NDArray[np.float64]], NDArray[np.float64]] = np.tanh, # activation function
     ):
@@ -43,22 +43,57 @@ class LSTMScratch():
         # kernel : (input_dim × 4 × units) 
         # recurrent kernel : (units × 4 × units)
         # bias : (4 × units)
-        self.kernel, self.recurrent_kernel, self.bias = keras_weight
         # f = sigmoid(Wf * input + Uf * ht-1 + bf)
         # i = sigmoid(Wi * input + Ui * ht-1 + bi)
         # ~c = activation_func(Wc * input + Uc * ht-1 + bc)
         # c = f * ct-1 + i * ~c
         # o = sigmoid(Wo * input + Uo * ht-1 + bo)
         # h = ot * activation_func(c)
-        print(1)
+        self.units = units
+        self.kernel, self.recurrent_kernel, self.bias = keras_weight
+        self.h_t = []
+        self.c_t = []
+        self.activation = activation
 
-    def forward(self):
-        timestep = 1
+    def fit(self, input_feature: NDArray[np.float64]):
+        self.input_feature = input_feature
 
-        pass
+    def predict(self, input_feature):
+        if isinstance(input_feature, tf.Tensor):
+            input_feature = input_feature.numpy() # type: ignore
+        sample = input_feature.shape[0]
+        timestep = input_feature.shape[1]
+        for i in range(sample):
+            current_sample = input_feature[i]
+            self.temp_h = [np.zeros(self.units)]
+            self.temp_c = [np.zeros(self.units)]
+
+            for j in range(timestep):
+                h_t, c_t = self.calc(current_sample[j]) 
+                self.temp_h.append(h_t)
+                self.temp_c.append(c_t)
+
+            self.h_t.append(self.temp_h[-1])
+            self.c_t.append(self.temp_c[-1])
+        return self.h_t
+
+    def calc(self, x_t: NDArray[np.float64]):
+        w_dot_x = x_t @ self.kernel
+        u_dot_h = self.temp_h[-1] @ self.recurrent_kernel
+        result = w_dot_x + u_dot_h + self.bias 
+        k_i, k_f, k_c, k_o = np.split(result, 4, axis=0)
+
+        i_t, f_t, o_t = self.sigmoid(k_i), self.sigmoid(k_f), self.sigmoid(k_o)
+
+        c_tilde = self.activation(k_c)
+        c_t = f_t * self.temp_c[-1] + i_t * c_tilde
+        
+        h_t = o_t * self.activation(c_t)
+
+        return h_t, c_t     
 
     def sigmoid(self, x):
-        return x
+        return 1/(1+np.exp(-x))
 
 
 '''
